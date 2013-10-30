acc6d58f2bb29e914b7bf514afe8e8eb47813110
github username <github email address>
2013-10-30 14:49:41 -0400

    the files dan so badly needs

--- bin/trial.rb
+++ bin/trial.rb
@@ -1,12 +1,40 @@
 require_relative '../config/environment'
 
+# client = Octokit::Client.new :netrc => true
+# client.login
+
 client = Octokit::Client.new \
-  :login => MY_LOGIN,
-  :password => MY_PASSWORD
+  :login    => 'mikespangler',
+  :password => 'github55'
 
 user = client.user
+user.login
 
-puts "FLATIRON REPO NAMES"
-user.rels[:organizations].get.data[0][:rels][:repos].head.data.each do |repo|
-  puts "#{repo.name} | #{repo.full_name}"
+# puts "FLATIRON REPO NAMES"
+# user.rels[:organizations].get.data[0][:rels][:repos].head.data.each do |repo|
+# #   puts "#{repo.name} | #{repo.full_name}"
+# # end
+
+repos = client.list_repos.collect do |repo|
+  repo.name
 end
+
+puts repos
+
+org_repos = client.organization_repositories('flatiron-school').collect do |repo|
+  repo.name
+end
+
+
+puts ""
+puts org_repos
+
+# client.organization_events('flatiron-school').each do |repo|
+#   puts "#{repo.name} | #{repo.full_name}"
+# end
+
+
+binding.pry
+# user is a sawyer agent object
+# repos are sawyer resource objects
+# 
